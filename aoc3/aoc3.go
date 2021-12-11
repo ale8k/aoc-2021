f2f8e2d8f83ebab100239b1838e9188dbd39060f
ale8k <alexanderkilroy@outlook.com>
2021-12-11 13:04:27 +0000

    aoc3p1

--- aoc3/aoc3.go
+++ aoc3/aoc3.go
@@ -1,63 +1,41 @@
 package aoc3
 
 import (
-	"fmt"
-	"math"
+	"errors"
 	"strconv"
 	"strings"
 )
 
-func parseBitMatrix(data []string) [][]int {
-	bitMatrix := make([][]int, 0)
+func parseBitMatrix(data []string) [][]string {
+	bitMatrix := make([][]string, 0)
 	for _, v := range data {
 		bitLine := strings.Split(v, "")
-		bitLineAtoI := make([]int, 0, 10)
-		for _, v := range bitLine {
-			k, _ := strconv.Atoi(v)
-			bitLineAtoI = append(bitLineAtoI, k)
-		}
-		bitMatrix = append(bitMatrix, bitLineAtoI)
+
+		bitMatrix = append(bitMatrix, bitLine)
 	}
 	return bitMatrix
 }
 
-func getIterationCount(matrix [][]int) int {
+func getIterationCount(matrix [][]string) (int, error) {
 	var bitLineIterations int
 	if len(matrix[0]) > 0 {
-		bitLineIterations = len(matrix[0]) - 1
+		bitLineIterations = len(matrix[0])
 	} else {
-		return -1
+		return -1, errors.New("iteration count cannot be 0")
 	}
-	return bitLineIterations
-}
-
-// Parses bit array to int (ignores idea of whether it is short, etc, whatnot)
-// just runs against each bits positional value
-func parseBitArrayToInt(bitArr []int) int {
-	finalInt := 0
-	length := len(bitArr) - 1
-	iterator := 0
-	for i := length; i > 0; i-- {
-		valueToAdd := math.Pow(2, float64(iterator))
-		if bitArr[i] == 1 {
-			finalInt += int(valueToAdd)
-		}
-		iterator++
-
-	}
-	return finalInt
+	return bitLineIterations, nil
 }
 
 // Convenience to just flip the entire arr
-func flipBitArr(bitArr []int) []int {
-	copi := make([]int, len(bitArr))
+func flipBitArr(bitArr []string) []string {
+	copi := make([]string, len(bitArr))
 	copy(copi, bitArr)
 
 	for i, v := range copi {
-		if v == 0 {
-			copi[i] = 1
+		if v == "0" {
+			copi[i] = "1"
 		} else {
-			copi[i] = 0
+			copi[i] = "0"
 		}
 	}
 	return copi
@@ -65,10 +43,15 @@ func flipBitArr(bitArr []int) []int {
 
 func AOC3P1(data []string) int {
 	bitMatrix := parseBitMatrix(data)
-	gammaMostCommon := make([]int, 0)
+	gammaMostCommon := make([]string, 0)
+	iterationCount, err := getIterationCount(bitMatrix)
 
-	for i := 0; i < len(bitMatrix[0]); i++ {
-		row := make([]int, 0)
+	if err != nil {
+		return -1
+	}
+
+	for i := 0; i < iterationCount; i++ {
+		row := make([]string, 0)
 
 		for j := 0; j < len(bitMatrix); j++ {
 			row = append(row, bitMatrix[j][i])
@@ -77,21 +60,26 @@ func AOC3P1(data []string) int {
 		zeroes := 0
 		ones := 0
 		for _, val := range row {
-			if val == 1 {
+			if val == "1" {
 				ones += 1
 			} else {
 				zeroes += 1
 			}
 		}
-
-		if zeroes < ones {
-			gammaMostCommon = append(gammaMostCommon, 1)
+		if ones > zeroes {
+			gammaMostCommon = append(gammaMostCommon, "1")
 		} else {
-			gammaMostCommon = append(gammaMostCommon, 0)
+			gammaMostCommon = append(gammaMostCommon, "0")
 		}
+
 	}
-	gam := parseBitArrayToInt(gammaMostCommon) >> 1
-	ep := parseBitArrayToInt((flipBitArr(gammaMostCommon))) >> 1
-	fmt.Println(gam, ep)
-	return 69
+
+	// Reverse did not add trailers in my original solution
+	gam, _ := strconv.ParseInt(strings.Join(gammaMostCommon, ""), 2, 64)
+	ep, _ := strconv.ParseInt(strings.Join(flipBitArr(gammaMostCommon), ""), 2, 64)
+	return int(gam * ep)
+}
+
+func AOC3P2(data []string) int {
+	return 10
 }
--- aoc3/aoc3_test.go
+++ aoc3/aoc3_test.go
@@ -1,10 +1,77 @@
 package aoc3
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/ale8k/aoc2021/utils"
 )
+
+func Test_parseBitMatrix(t *testing.T) {
+	type args struct {
+		data []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseBitMatrix(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseBitMatrix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getIterationCount(t *testing.T) {
+	type args struct {
+		matrix [][]string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getIterationCount(tt.args.matrix)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getIterationCount() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("getIterationCount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_flipBitArr(t *testing.T) {
+	type args struct {
+		bitArr []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := flipBitArr(tt.args.bitArr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("flipBitArr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func TestAOC3P1(t *testing.T) {
 	type args struct {
@@ -13,17 +80,59 @@ func TestAOC3P1(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
+		want int
 	}{
 		{
-			"it runs",
+			"it passes aoc3p1",
 			args{data: func() []string {
 				return utils.GrabDataFromFile("./aoc3input.txt")
 			}()},
+			2250414,
+		},
+		{
+			"it passes aoc3p1 with examplar values",
+			args{data: []string{
+				"00100",
+				"11110",
+				"10110",
+				"10111",
+				"10101",
+				"01111",
+				"00111",
+				"11100",
+				"10000",
+				"11001",
+				"00010",
+				"01010",
+			}},
+			198,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			AOC3P1(tt.args.data)
+			if got := AOC3P1(tt.args.data); got != tt.want {
+				t.Errorf("AOC3P2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAOC3P2(t *testing.T) {
+	type args struct {
+		data []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AOC3P2(tt.args.data); got != tt.want {
+				t.Errorf("AOC3P2() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
