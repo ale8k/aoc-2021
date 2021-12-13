d9417bd8f9dccc0bfc3bc8d7f5e94d4a0f1b75a2
ale8k <alexanderkilroy@outlook.com>
2021-12-13 21:34:07 +0000

    Mhm.. lol

--- aoc5/aoc5.go
+++ aoc5/aoc5.go
@@ -9,10 +9,51 @@ import (
 	"strings"
 )
 
-// An entry like 1,1 -> 1,3 covers points 1,1, 1,2, and 1,3.
-// An entry like 9,7 -> 7,7 covers points 9,7, 8,7, and 7,7.
-
+// Plots a bunch of coordinates onto a matrix
 func AOC5P1(data *os.File) {
+	matrix, lineCoords := makeMatrixAndLines(data)
+
+	for _, coord := range lineCoords {
+		x := coord[0]
+		y := coord[1]
+		matrix[y][x]++
+	}
+
+	crossedOverLines := 0
+
+	// Gets all co-ordinates of value 2 or greater
+	for _, yAxis := range matrix {
+		for _, coordinate := range yAxis {
+			if coordinate > 1 {
+				crossedOverLines++
+			}
+		}
+	}
+	// 7370 tried, wrong, too low
+	fmt.Println(crossedOverLines)
+
+}
+
+func makeMatrixAndLines(data *os.File) ([][]int, [][]int) {
+	lines := getHorizontalLines(data)
+	largestX := getLargestXY(lines, 0)
+	largestY := getLargestXY(lines, 1)
+
+	matrix := make([][]int, largestY+1)
+	fillMatrix(matrix, largestX+1, largestY+1)
+	return matrix, lines
+}
+
+// Filles a matrix of ints to default values of x/y size (presumes y constructed)
+func fillMatrix(matrix [][]int, x int, y int) {
+	for i := 0; i < y; i++ {
+		k := make([]int, x)
+		matrix[i] = k
+	}
+}
+
+// Gets all co-ordinatates to plot the lines on a matrix
+func getHorizontalLines(data *os.File) [][]int {
 	reader := bufio.NewReader(data)
 	lines := make([][]int, 0)
 	for {
@@ -33,7 +74,7 @@ func AOC5P1(data *os.File) {
 
 		// Handle only horizontal for first part
 		if fromX == toX {
-			if fromY < toY {
+			if fromY <= toY {
 				for fromY != toY {
 					lines = append(lines, []int{fromX, fromY})
 					fromY++
@@ -55,10 +96,19 @@ func AOC5P1(data *os.File) {
 					lines = append(lines, []int{fromX, fromY})
 					fromX--
 				}
-
 			}
 		}
-
 	}
-	fmt.Println(lines)
+	return lines
+}
+
+// Gets argest x or y co-ord, 0 = x, 1 = y
+func getLargestXY(slice [][]int, xOrY int) int {
+	currentLargest := 0
+	for _, v := range slice {
+		if currentLargest < v[xOrY] {
+			currentLargest = v[xOrY]
+		}
+	}
+	return currentLargest
 }
